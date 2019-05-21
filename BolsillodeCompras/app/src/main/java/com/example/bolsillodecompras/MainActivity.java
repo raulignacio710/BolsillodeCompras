4bab9c096d8ee755b2e9bc7f7e758d7fb1a013a7
Raul <raul.flores.arenas@gmail.com>
2019-05-21 00:04:52 -0400

    Bolsillo de Compras v1.1

--- BolsillodeCompras/app/src/main/java/com/example/bolsillodecompras/MainActivity.java
+++ BolsillodeCompras/app/src/main/java/com/example/bolsillodecompras/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bolsillodecompras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mostrar (View v){
+        Toast.makeText(this,"JIJI me presionaste", Toast.LENGTH_LONG).show();
+        Intent nueva = new Intent(this,ListaMain.class);
+        startActivity(nueva);
     }
 }
